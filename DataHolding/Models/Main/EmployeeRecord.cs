078039e9751852d5b77e8d341db909056a69d59a
Anish Adhikari <73144665+anish-aries@users.noreply.github.com>
2025-06-03 08:27:12 +0545

    Update EmployeeRecord.cs

--- DataHolding/Models/Main/EmployeeRecord.cs
+++ DataHolding/Models/Main/EmployeeRecord.cs
@@ -9,13 +9,13 @@ namespace DataHolding.Models.Main
         [Key]
         public int Id{ get; set; }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
-        public string? Address { get; set; }
+        public string Address { get; set; }
 
-        public string? District { get; set; }
-        public string? Phone { get; set; }
-        public DateTime? Dob { get; set; }
+        public string District { get; set; }
+        public string Phone { get; set; }
+        public DateTime Dob { get; set; }
     }
 
 }
